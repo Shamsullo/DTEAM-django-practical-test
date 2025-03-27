0f9068313643ccb6688282366b7af082a151fb14
Shams <shamsulloismatov@gmail.com>
2025-03-27 20:34:31 +0500

    Add PDF generation template and view for CV export. Not finished (weasyprint adaptation left)
    This commit creates a new PDF template (`cv_pdf.html`) for exporting CVs in a printable format. It also introduces a `CVDetailPDFView` and updates the URLs and frontend to support PDF downloads. Placeholder implementation for the view is included with commented-out code for further enhancements.

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CVListView, CVDetailView
+from .views import CVDetailPDFView, CVListView, CVDetailView
 
 urlpatterns = [
     path('', CVListView.as_view(), name='cv_list'),
     path('cv/<int:pk>/', CVDetailView.as_view(), name='cv_detail'),
+    path('cv/<int:pk>/pdf/', CVDetailPDFView.as_view(), name='cv_pdf'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Prefetch
-from django.views.generic import DetailView
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+# from django_weasyprint import WeasyTemplateResponseMixin
+# from django.http import HttpResponseServerError
 
 from .models import CV, Contact, Project, Skill
 
@@ -32,3 +33,28 @@ class CVDetailView(DetailView):
                      queryset=Project.objects.order_by('-start_date')),
             Prefetch('contacts', queryset=Contact.objects.all())
         )
+
+
+class CVDetailPDFView(DetailView):
+    pass
+# class CVDetailPDFView(WeasyTemplateResponseMixin, DetailView):
+#     model = CV
+#     template_name = 'main/cv_pdf.html'
+#     pdf_filename = 'cv_{pk}.pdf'
+#
+#     def get_pdf_filename(self):
+#         return f'cv_{self.object.first_name}_{self.object.last_name}.pdf'.lower()
+#
+#     def get_queryset(self):
+#         return CV.objects.prefetch_related(
+#             Prefetch('skills', queryset=Skill.objects.all()),
+#             Prefetch('projects',
+#                      queryset=Project.objects.order_by('-start_date')),
+#             Prefetch('contacts', queryset=Contact.objects.all())
+#         )
+#
+#     def get(self, request, *args, **kwargs):
+#         try:
+#             return super().get(request, *args, **kwargs)
+#         except Exception as e:
+#             return HttpResponseServerError(f"Error generating PDF: {str(e)}")
