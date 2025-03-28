a3083369f2d32b19e02bab980b69226c0d696309
Shams <shamsulloismatov@gmail.com>
2025-03-28 05:22:35 +0500

    Task 5. Add settings view and template to display Django settings.
    Introduced a `SettingsView` and corresponding template (`settings.html`) to render key Django settings for debugging and informational purposes. Added a context processor to make settings globally accessible in templates and updated navigation to include a link to the settings page.

--- CVProject/urls.py
+++ CVProject/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from CVProject.views import SettingsView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('audit/', include('audit.urls')),
     path('api/', include('main.api.urls'), name='api_cv'),
+    path('settings/', SettingsView.as_view(), name='settings'),
 ]
